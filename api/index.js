941657a65cc33b3a632f4ea1176fed8f3b04ca01
andjelacorbic <andjelacorbic2001@gmail.com>
2024-01-30 15:07:26 +0100

    kreiranje fje - errorHandler

--- api/index.js
+++ api/index.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.MONGO).then(
 
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server je aktivan na portu 3000!');
@@ -20,3 +21,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
