701bb5929596d0a1a8a74fd0bd7febb8cd3f442e
andjelacorbic <andjelacorbic2001@gmail.com>
2024-01-31 15:22:26 +0100

    header update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
   const path = useLocation().pathName;
+  const location = useLocation();
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className='border-b-2'> 
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold'>
@@ -23,12 +26,42 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='md:order-2'>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar
+                alt='user'
+                img={currentUser.profilePicture}
+                rounded
+              
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profil</Dropdown.Item>
+            </Link>
+            <DropdownDivider />
+            <Dropdown.Item>Odjavi me</Dropdown.Item>
+          </Dropdown>
+        ):
+        (
+
         <Link to='/sign-in'>
           <Button color='gray'>
-            Sign In
+            Uloguj se
           </Button>
           <Navbar.Toggle /> 
         </Link>
+
+        )
+      
+      }
       </div>
         <Navbar.Collapse>
           <Navbar.Link active={path === "/naslovna"} as={'div'}>
