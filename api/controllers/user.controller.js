adceeaad0c51a6bb104e322ba5333458696584da
andjelacorbic <andjelacorbic2001@gmail.com>
2024-02-02 19:47:43 +0100

    fja update korisnika

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,48 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+
 export const test = (req, res) => {
     res.json('API radi! Ura!');
+};
+
+
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.userId){
+        return next(errorHandler(403, 'Izmena nije moguća'));
+    }
+    if (req.body.password){
+        if(req.body.password.lenght < 4){
+            return next(errorHandler(400, 'Lozinka mora sadržati najmanje 4 karaktera'));
+        }
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    if (req.body.username){
+        if(req.body.username.lenght < 5 || req.body.username.lenght > 15){
+            return next(errorHandler(400, 'Korisnik mora imati između 5 i 15 karaktera'));
+        }
+        if(req.body.username.includes(' ')){
+            return next(errorHandler(400, 'Space nije dozvoljen'));
+        }
+        if(req.body.username !== req.body.username.toLowerCase()){
+            return next(errorHandler(400, 'Nisu dozvoljena velika slova'));
+        }
+        if (!req.body.username.match(/^[a-zA-Z0-9]+$/)){
+            return next(errorHandler(400, 'Korisničko ime može sadržati samo slova i brojeve'));
+        }
+        try {
+            const updateUser = await User.findByIdAndUpdate(req.params.userId, {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    profilePicture: req.body.profilePicture,
+                    password: req.body.password,
+                },
+            }, { new: true });
+            const { password, ...rest } = updateUser._doc;
+            res.status(200).json(rest);
+        } catch (error) {
+            next(error);
+        }
+    }
 };
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Button, TextInput } from 'flowbite-react';
 
 export default function DashProfile() {
   const {currentUser} = useSelector(state => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if(file){
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile] );
+
+  const uploadImage = async () => {
+    console.log('Učitavanje slike...');
+  };
+  
   return (
     <div className='max-w-lg  p-3 w-full'>
       <h1 className='my-7 text-left font-semibold text-3xl'>Moj profil</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32'>
-        <img src={currentUser.profilePicture} alt='korisnik' className='rounded-full w-full h-full object-cover border-10 border-[black]' />
+        <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden />
+        <div className='w-32 h-32' onClick={() => filePickerRef.current.click ()}>
+        <img src={imageFileUrl || currentUser.profilePicture} alt='korisnik' className='rounded-full w-full h-full object-cover border-10 border-[black]' />
         </div>
         <TextInput type='text' id='username' placeholder='korisničko ime' defaultValue={currentUser.username}/>
         <TextInput type='email' id='email' placeholder='email adresa' defaultValue={currentUser.email}/>
