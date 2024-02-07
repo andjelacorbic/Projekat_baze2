705144d52df2c76efe864fe38880a0aa17fa13e2
andjelacorbic <andjelacorbic2001@gmail.com>
2024-02-07 15:57:35 +0100

    korisnici

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -9,23 +8,48 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import { app } from '../firebase';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-export default function CreatePost() {
+export default function UpdatePost() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({});
   const [publishError, setPublishError] = useState(null);
+  const { postId } = useParams();
 
   const navigate = useNavigate();
+    const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    try {
+      const fetchPost = async () => {
+        const res = await fetch(`/api/post/getposts?postId=${postId}`);
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+          setPublishError(data.message);
+          return;
+        }
+        if (res.ok) {
+          setPublishError(null);
+          setFormData(data.posts[0]);
+        }
+      };
+
+      fetchPost();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [postId]);
 
   const handleUpdloadImage = async () => {
     try {
-      if (!file){
-        setImageUploadError('Unesite sliku.');
+      if (!file) {
+        setImageUploadError('Molimo vas izaberite sliku');
         return;
       }
       setImageUploadError(null);
@@ -41,7 +65,7 @@ export default function CreatePost() {
           setImageUploadProgress(progress.toFixed(0));
         },
         (error) => {
-          setImageUploadError('Slika nije postavljena.');
+          setImageUploadError('Slika nije učitana');
           setImageUploadProgress(null);
         },
         () => {
@@ -52,18 +76,17 @@ export default function CreatePost() {
           });
         }
       );
-    } catch {
-      setImageUploadError('Slika nije objavljena');
+    } catch (error) {
+      setImageUploadError('Slika nije učitana');
       setImageUploadProgress(null);
       console.log(error);
     }
   };
-  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('/api/post/create', {
-        method: 'POST',
+      const res = await fetch(`/api/post/updatepost/${formData._id}/${currentUser._id}`, {
+        method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
@@ -74,7 +97,7 @@ export default function CreatePost() {
         setPublishError(data.message);
         return;
       }
-      
+
       if (res.ok) {
         setPublishError(null);
         navigate(`/post/${data.slug}`);
@@ -83,27 +106,27 @@ export default function CreatePost() {
       setPublishError('Greška');
     }
   };
-
   return (
-   
-       <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-      <h1 className='text-center text-3xl my-7 font-semibold'>Dodaj post</h1>
-      <form className='flex flex-col gap-4'onSubmit={handleSubmit}>
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+      <h1 className='text-center text-3xl my-7 font-semibold'>Izmeni</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-4 sm:flex-row justify-between'>
           <TextInput
             type='text'
-            placeholder='Naslov'
+            placeholder='Title'
             required
             id='title'
             className='flex-1'
             onChange={(e) =>
               setFormData({ ...formData, title: e.target.value })
             }
+            value={formData.title}
           />
           <Select
             onChange={(e) =>
               setFormData({ ...formData, category: e.target.value })
             }
+            value={formData.category}
           >
             <option value='uncategorized'>Izaberi kategoriju</option>
             <option value='Vesti dana'>Vesti dana</option>
@@ -111,7 +134,7 @@ export default function CreatePost() {
             <option value='Horoskop'>Horoskop</option>
           </Select>
         </div>
-        <div className='flex gap-4 items-center justify-between border-4 border-gray-500 border-dotted p-3'>
+        <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
           <FileInput
             type='file'
             accept='image/*'
@@ -119,14 +142,13 @@ export default function CreatePost() {
           />
           <Button
             type='button'
-            color='gray'
+            gradientDuoTone='purpleToBlue'
             size='sm'
             outline
             onClick={handleUpdloadImage}
             disabled={imageUploadProgress}
           >
-           
-           {imageUploadProgress ? (
+            {imageUploadProgress ? (
               <div className='w-16 h-16'>
                 
                   value={imageUploadProgress}
@@ -134,8 +156,8 @@ export default function CreatePost() {
                 
               </div>
             ) : (
-              'Dodaj sliku'
-            )}         
+              'Upload Image'
+            )}
           </Button>
         </div>
         {imageUploadError && <Alert color='failure'>{imageUploadError}</Alert>}
@@ -148,16 +170,16 @@ export default function CreatePost() {
         )}
         <ReactQuill
           theme='snow'
-          placeholder='Napiši nešto...'
+          value={formData.content}
+          placeholder='Write something...'
           className='h-72 mb-12'
           required
           onChange={(value) => {
             setFormData({ ...formData, content: value });
           }}
-          
         />
-        <Button type='submit' color='gray'>
-          Objavi
+        <Button type='submit' gradientDuoTone='purpleToPink'>
+          Update post
         </Button>
         {publishError && (
           <Alert className='mt-5' color='failure'>
@@ -166,7 +188,5 @@ export default function CreatePost() {
         )}
       </form>
     </div>
-   
   );
 }
-
